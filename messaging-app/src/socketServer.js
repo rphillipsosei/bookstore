3219a492256a9b56ddd3a19cba4fcc3a23582d8a
rphillipsosei <rhodaphillipsosei@gmail.com>
2022-02-19 00:33:55 +0000

    Listen for websocket cxns

--- messaging-app/src/socketServer.js
+++ messaging-app/src/socketServer.js
@@ -1,1 +1,9 @@
 const socketio = require("socket.io");
+
+//listen for websocket connections
+const listen = function(httpServer){
+const server = socketio(httpServer)
+return server
+};
+
+module.exports = { listen };
